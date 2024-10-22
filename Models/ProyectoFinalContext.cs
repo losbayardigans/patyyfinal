6a7c7684466b993c1678b215a6ffac555e60d4f1
fcu2134 <facundo.marin.moscol@cftmail.cl>
2024-10-21 18:30:17 -0600

    actualizacion error

--- Models/ProyectoFinalContext.cs
+++ Models/ProyectoFinalContext.cs
@@ -45,8 +45,12 @@ public partial class ProyectoFinalContext : DbContext
     public virtual DbSet<Proveedor> Proveedors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=proyecto_final;uid=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.32-mariadb"));
+    {
+        if (!optionsBuilder.IsConfigured!)
+        {
+
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
