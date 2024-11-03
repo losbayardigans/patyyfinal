9a97f707846793b1fe2f0da7771789850d4250b0
fcu2134 <facundo.marin.moscol@cftmail.cl>
2024-11-03 02:28:29 -0300

    controlador y vista pedido

---
+++ Controllers/PedidoesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using patyy.Models;
+
+namespace patyy.Controllers
+{
+    public class PedidoesController : Controller
+    {
+        private readonly ProyectoFinalContext _context;
+
+        public PedidoesController(ProyectoFinalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pedidoes
+        public async Task<IActionResult> Index()
+        {
+            var proyectoFinalContext = _context.Pedidos.Include(p => p.ClienteIdClienteNavigation);
+            return View(await proyectoFinalContext.ToListAsync());
+        }
+
+        // GET: Pedidoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .Include(p => p.ClienteIdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.IdPedido == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteIdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente");
+            return View();
+        }
+
+        // POST: Pedidoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPedido,Total,MetodoPago,EstadoPedido,ProductosIdProducto,ClienteIdCliente,FechaPedido")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteIdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente", pedido.ClienteIdCliente);
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteIdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente", pedido.ClienteIdCliente);
+            return View(pedido);
+        }
+
+        // POST: Pedidoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPedido,Total,MetodoPago,EstadoPedido,ProductosIdProducto,ClienteIdCliente,FechaPedido")] Pedido pedido)
+        {
+            if (id != pedido.IdPedido)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.IdPedido))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteIdCliente"] = new SelectList(_context.Clientes, "IdCliente", "IdCliente", pedido.ClienteIdCliente);
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .Include(p => p.ClienteIdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.IdPedido == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: Pedidoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido != null)
+            {
+                _context.Pedidos.Remove(pedido);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedidos.Any(e => e.IdPedido == id);
+        }
+    }
+}
